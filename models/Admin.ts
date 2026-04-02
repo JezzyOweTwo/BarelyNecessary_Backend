a355edaf2d20f1ca7a763cef933a3f8236d86911
JezzyOweTwo <fjeziah@gmail.com>
2026-04-01 20:47:19 -0400

    removed unncessary shit

--- models/Admin.ts
+++
@@ -1,8 +0,0 @@
-// import { getModelForClass } from '@typegoose/typegoose';
-// import { User, UserPreferences } from './User';
-// import mongoose from 'mongoose';
-
-// class Admin extends User{}
-
-// // exports the user model for future usage
-// export const AdminModel = getModelForClass(Admin); // UserModel is a regular Mongoose Model with correct types
--- models/Client.ts
+++
@@ -1,7 +0,0 @@
-// import { prop, getModelForClass } from '@typegoose/typegoose';
-// import { User, UserPreferences } from './User';
-
-// class Client extends User{}
-
-// // exports the client model for future usage
-// export const ClientModel = getModelForClass(Client); // UserModel is a regular Mongoose Model with correct types
